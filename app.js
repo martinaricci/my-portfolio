a49222c478a8033d01c3f9bbabfeb1c36454bda1
Martina Ricci <martinaricci93@live.it>
2018-06-01 12:01:00 +1000

    working on css animations

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('hello')
+
+// $('.mywork-boxes').on('hover', 'li', function(e) {
+//     // debugger
+//     $(e.target).find('h2').addClass('.make-visible')
+//     // $(e.target).addClass('h2')
+// })
+
+$(function () {
+    var controller = new ScrollMagic.Controller();
+
+    var wipeAnimation = new TimelineMax()
+        .fromTo("section.mywork-container", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
+        .fromTo("section.aboutme-container", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})
+        .fromTo("section.contact-container", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone});
+
+        new ScrollMagic.Scene({
+            triggerElement: "#entire-page",
+            triggerHook: "onLeave",
+            duration: "300%"
+        })
+        .setPin("#entire-page")
+        .setTween(wipeAnimation)
+        .addIndicators()
+        .addTo(controller);
+});
